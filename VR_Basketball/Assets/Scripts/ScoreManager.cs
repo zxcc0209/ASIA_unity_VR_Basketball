3a5b5ef565d9f6bb02859cbe91818d7fb2573e6b
zxcc0209 <71679895+zxcc0209@users.noreply.github.com>
2020-12-22 12:00:44 +0800

    分數觸發

---
+++ VR_Basketball/Assets/Scripts/ScoreManager.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class ScoreManager : MonoBehaviour
+{
+    [Header("分數介面")]
+    public Text text;
+    [Header("分數")]
+    public int score;
+    private void OnTriggerEnter(Collider other)
+    {
+        print(other.name);
+    }
+}

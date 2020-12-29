ad24e42beae2b5461819d1549aeb25608fb2a701
zxcc0209 <71679895+zxcc0209@users.noreply.github.com>
2020-12-29 10:59:20 +0800

    三分球觸發

--- VR_Basketball/Assets/Scripts/ScoreManager.cs
+++ VR_Basketball/Assets/Scripts/ScoreManager.cs
@@ -4,11 +4,38 @@ using UnityEngine.UI;
 public class ScoreManager : MonoBehaviour
 {
     [Header("分數介面")]
-    public Text text;
+    public Text textScroe;
+
     [Header("分數")]
     public int score;
+
+    [Header("投進分數")]
+    public int scorein;
+
+
     private void OnTriggerEnter(Collider other)
     {
-        print(other.name);
+        if (other.tag == "籃球")
+        {
+            AddScroe();
+        }
+        if (other.transform.root.name =="Player")
+        {
+            scorein = 3;
+        }
+    }
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.transform.root.name =="Player")
+        {
+            scorein = 2;
+
+        }
+    }
+    private void AddScroe()
+    {
+        score += scorein;
+        textScroe.text = "分數:" + score;
+
     }
 }
